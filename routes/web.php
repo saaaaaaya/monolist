3fa59fa616493fff4aec09a352505a9c93ebd532
saaya.akashi <ec2-user@ip-172-31-17-77.us-east-2.compute.internal>
2018-06-14 07:05:20 +0000

    rakuten api

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@
     // Login authentication
     Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
     Route::post('login', 'Auth\LoginController@login')->name('login.post');
-    Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');    
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});    
